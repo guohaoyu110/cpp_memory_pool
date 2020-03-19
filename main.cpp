1a56bd482dc8194b5f9453be9b0778487450db1e
guohaoyu110 <guohaoyu110@gmail.com>
2020-03-19 14:24:53 -0700

    allocator modified

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//在一个高可用的软件中，如果我们仅仅单纯的向操作系统去申请内存，当出现内存不足时就退出软件，是明显不合理的。
+// 在一个高可用的软件中，如果我们仅仅单纯的向操作系统去申请内存，当出现内存不足时就退出软件，是明显不合理的。
 // 应该考虑在内存不足的时候，考虑如何管理并且优化自己已经使用的内存，这样才能使得软件更加可用。
 // 实现一个内存池，并且使用一个栈结构来测试我们的内存池提供的分配性能。
 '''
@@ -12,32 +12,41 @@
 #include <vector>
 #include "MemoryPool.hpp"  //MemoryPool<T>
 #include "StackAlloc.hpp" //StackAlloc<T, Alloc>
-using namespace std;
+
 //插入元素个数 
 #define ELEMS 10000000
 //重复次数
 #define REPS 100
 
-int main()
-{
+int main(){
     clock_t start;
 
     //使用STL 默认分配器
     StackAlloc<int,allocator<int>> stackDefault;
     start = clock();
-    for(int j = 0;j<REPS; j++)
-    {
+    for(int j = 0;j<REPS; j++){
         assert(stackDefault.empty());
         for(int i = 0;i<ELEMS;i++)
             stackDefault.push(i);
         for(int i =0;i<ELEMS;i++)
             stackDefault.pop();
     }
-    cout<<"Default Allocator Time: ";
-    cout<<(((double)clock() - start)/CLOCKS_PER_SEC)<<endl;
+    std::cout<<"Default Allocator Time: ";
+    std::cout<<(((double)clock() - start)/CLOCKS_PER_SEC)<<endl;
 
     //使用内存池
     StackAlloc<int, MemoryPool<int>> stackPool;
     start = clock();
+    for(int j=0; j<REPS;j++){
+        assert(stackPool.empty());
+        for(int i=0;i<ELEMS; i++){
+            stackPool.push(i);
+        }
+        for (int i=0;i<ELEMS;i++)
+            stackPool.pop();
+    }
+    std::cout << "MemoryPool Allocator Time: ";
+    std::cout << (((double)clock() - start) / CLOCKS_PER_SEC) << "\n\n";
+    return 0;
     
 }
